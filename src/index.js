3e9ff6b8faf6fc39a8d723b1d3e20446f29c4155
Zou Jiyun <zoujiyun@tinman.cn>
2021-06-15 16:34:35 +0800

    阅读源码前准备

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return state+1;
+    default:
+      return state;  
+  }
+}
+function Counter() {
+  debugger
+  const [number, setNumber] = React.useReducer(reducer, 0)
+  return (<div>
+    {number}
+    <button onClick={() => setNumber({type: 'add'})}>Add</button>
+  </div>)
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
+
+// Fiber
+const workInProgress = {
+  tag: 2, // Fiber的类型
+}
